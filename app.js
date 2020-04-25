c3798ee92d227670d0fb14b636fd7509cf495f69
MoeTech <jemkarry@live.com>
2020-04-25 05:00:40 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+//value in kelvin to be converted
+const kelvin = 0;
+console.log(`Kelvin is ${kelvin}`);
+
+//in kelvin
+let celsius = kelvin - 273;
+console.log(`celsius is ${celsius}`);
+
+//in Fahrenhite
+let Fahrenheit = celsius * (9/5) + 32;
+Fahrenheit = Math.floor(Fahrenheit);
+console.log(`The temperature is ${Fahrenheit} degrees Fahrenheit.`);
+
+
+
+
+
