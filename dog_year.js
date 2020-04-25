9dac27bc500385b2e5b55aaca3a9faec4accf985
MoeTech <jemkarry@live.com>
2020-04-25 05:35:53 -0400

    added new js file

---
+++ dog_year.js
@@ -0,0 +1,25 @@
+//my age 
+let myAge = 32;
+
+//early years
+let earlyYears = 2;
+
+earlyYears *= 10.5;
+
+//deduct first 2 years
+laterYears = myAge - 2;
+
+//multiplying last years
+laterYears *= 4;
+
+console.log(earlyYears);
+console.log(laterYears);
+
+//full age
+myAgeInDogYears = earlyYears + laterYears;
+
+//My name in lower case
+let myName = 'Mohamed'.toLowerCase();
+
+console.log(`My name is ${myName}. I am HUMAN ${myAge} years old in human years which is DOG ${myAgeInDogYears} years old in dog years.`)
+
